5245cd0313017c1a5e962a7c8e1067192e53038c
chingle15 <tchinglewow@gmail.com>
2023-01-30 16:26:41 +0800

    add manifest

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { GiphyService } from './giphy.service';
+import { Gif, SearchCriteria } from './models';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+
+  gifs: Gif[] = []
+
+  constructor(private giphySvc: GiphyService) { }
+
+  onSearch(searchCriteria: SearchCriteria) {
+    console.info('>>> searchCriteria ', searchCriteria)
+    this.giphySvc.search(searchCriteria)
+      .then(result => {
+        console.info(">>> result: ", result)
+        this.gifs = result
+      })
+      .catch(error => {
+        console.error('>>> error: ', error)
+      })
+    console.info('-------- after calling giphySvc.search()')
+  }
+}
